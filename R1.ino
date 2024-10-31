5bb57b8ea487c13ee5287fae5a3494f3c6cc0b02
niska223 <140884526+niska223@users.noreply.github.com>
2024-10-31 22:06:07 +0530

    Add files via upload

---
+++ R1.ino
@@ -0,0 +1,38 @@
+#include <DS3231.h>
+
+int Relay = 4;
+
+DS3231  rtc(SDA, SCL);
+Time t;
+
+const int OnHour = 10;
+const int OnMin = 27;
+const int OffHour = 10;
+const int OffMin = 29;
+
+void setup() {
+  Serial.begin(115200);
+  rtc.begin();
+  pinMode(Relay, OUTPUT);
+  digitalWrite(Relay, LOW);
+}
+
+void loop() {
+  t = rtc.getTime();
+  Serial.print(t.hour);
+  Serial.print(" hour(s), ");
+  Serial.print(t.min);
+  Serial.print(" minute(s)");
+  Serial.println(" ");
+  delay (1000);
+  
+  if(t.hour == OnHour && t.min == OnMin){
+    digitalWrite(Relay,HIGH);
+    Serial.println("LIGHT ON");
+    }
+    
+    else if(t.hour == OffHour && t.min == OffMin){
+      digitalWrite(Relay,LOW);
+      Serial.println("LIGHT OFF");
+    }
+}
